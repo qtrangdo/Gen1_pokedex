37dada76966760c44c304aa0a9fb6f1ab9f2ff62
td94 <quynhtrangdo94@gmail.com>
2019-03-16 17:54:06 -0700

    Setup requestAllMons action, fix action typos

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,8 +1,16 @@
 import {
-  REQUEST_AllMONS_PENDING,
-  REQUEST_AllMONS_SUCCESS,
-  REQUEST_AllMONS_FAILED,
+  REQUEST_ALLMONS_PENDING,
+  REQUEST_ALLMONS_SUCCESS,
+  REQUEST_ALLMONS_FAILED,
   REQUEST_ONEMON_PENDING,
   REQUEST_ONEMON_SUCCESS,
   REQUEST_ONEMON_FAILED,
 } from './actionTypes';
+
+const requestAllMons = () => (dispatch) => {
+  dispatch({ type: REQUEST_ALLMONS_PENDING });
+  fetch('/api/pokemons')
+    .then(res => res.json())
+    .then(data => dispatch({ type: REQUEST_ALLMONS_SUCCESS, payload: data }))
+    .catch(error => dispatch({ type: REQUEST_ALLMONS_FAILED, payload: error }));
+}
